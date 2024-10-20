54cd6468e5fa7d967e25fc54e8bbd471be0c20b3
Symbolinker <symbolinker@gmail.com>
2024-10-20 20:39:59 +0200

    Issue #1, point 2: use `ConfigureAwait(false)` on all `await`s
    https://github.com/SymboLinker/AwaitMultiple/issues/1

--- AwaitMultiple/AwaitMultiple.cs
+++ AwaitMultiple/AwaitMultiple.cs
@@ -4,59 +4,67 @@
     {
         public static async Task<(T1, T2)> tasks<T1, T2>(Task<T1> task1, Task<T2> task2)
         {
-            await AggregateExceptions(task1, task2);
-            return (await task1, await task2);
+            await AggregateExceptions(task1, task2)
+                .ConfigureAwait(false);
+            return (task1.Result, task2.Result);
         }
 
         public static async Task<(T1, T2, T3)> tasks<T1, T2, T3>(
             Task<T1> task1, Task<T2> task2, Task<T3> task3)
         {
-            await AggregateExceptions(task1, task2, task3);
-            return (await task1, await task2, await task3);
+            await AggregateExceptions(task1, task2, task3)
+                .ConfigureAwait(false);
+            return (task1.Result, task2.Result, task3.Result);
         }
 
         public static async Task<(T1, T2, T3, T4)> tasks<T1, T2, T3, T4>(
             Task<T1> task1, Task<T2> task2, Task<T3> task3, Task<T4> task4)
         {
-            await AggregateExceptions(task1, task2, task3, task4);
-            return (await task1, await task2, await task3, await task4);
+            await AggregateExceptions(task1, task2, task3, task4)
+                .ConfigureAwait(false);
+            return (task1.Result, task2.Result, task3.Result, task4.Result);
 
         }
 
         public static async Task<(T1, T2, T3, T4, T5)> tasks<T1, T2, T3, T4, T5>(
             Task<T1> task1, Task<T2> task2, Task<T3> task3, Task<T4> task4, Task<T5> task5)
         {
-            await AggregateExceptions(task1, task2, task3, task4, task5);
-            return (await task1, await task2, await task3, await task4, await task5);
+            await AggregateExceptions(task1, task2, task3, task4, task5)
+                .ConfigureAwait(false);
+            return (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result);
 
         }
 
         public static async Task<(T1, T2, T3, T4, T5, T6)> tasks<T1, T2, T3, T4, T5, T6>(
             Task<T1> task1, Task<T2> task2, Task<T3> task3, Task<T4> task4, Task<T5> task5, Task<T6> task6)
         {
-            await AggregateExceptions(task1, task2, task3, task4, task5, task6);
-            return (await task1, await task2, await task3, await task4, await task5, await task6);
+            await AggregateExceptions(task1, task2, task3, task4, task5, task6)
+                .ConfigureAwait(false);
+            return (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result, task6.Result);
         }
 
         public static async Task<(T1, T2, T3, T4, T5, T6, T7)> tasks<T1, T2, T3, T4, T5, T6, T7>(
             Task<T1> task1, Task<T2> task2, Task<T3> task3, Task<T4> task4, Task<T5> task5, Task<T6> task6, Task<T7> task7)
         {
-            await AggregateExceptions(task1, task2, task3, task4, task5, task6, task7);
-            return (await task1, await task2, await task3, await task4, await task5, await task6, await task7);
+            await AggregateExceptions(task1, task2, task3, task4, task5, task6, task7)
+                .ConfigureAwait(false);
+            return (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result, task6.Result, task7.Result);
         }
 
         public static async Task<(T1, T2, T3, T4, T5, T6, T7, T8)> tasks<T1, T2, T3, T4, T5, T6, T7, T8>(
             Task<T1> task1, Task<T2> task2, Task<T3> task3, Task<T4> task4, Task<T5> task5, Task<T6> task6, Task<T7> task7, Task<T8> task8)
         {
-            await AggregateExceptions(task1, task2, task3, task4, task5, task6, task7, task8);
-            return (await task1, await task2, await task3, await task4, await task5, await task6, await task7, await task8);
+            await AggregateExceptions(task1, task2, task3, task4, task5, task6, task7, task8)
+                .ConfigureAwait(false);
+            return (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result, task6.Result, task7.Result, task8.Result);
         }
 
         public static async Task<(T1, T2, T3, T4, T5, T6, T7, T8, T9)> tasks<T1, T2, T3, T4, T5, T6, T7, T8, T9>(
             Task<T1> task1, Task<T2> task2, Task<T3> task3, Task<T4> task4, Task<T5> task5, Task<T6> task6, Task<T7> task7, Task<T8> task8, Task<T9> task9)
         {
-            await AggregateExceptions(task1, task2, task3, task4, task5, task6, task7, task8, task9);
-            return (await task1, await task2, await task3, await task4, await task5, await task6, await task7, await task8, await task9);
+            await AggregateExceptions(task1, task2, task3, task4, task5, task6, task7, task8, task9)
+                .ConfigureAwait(false);
+            return (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result, task6.Result, task7.Result, task8.Result, task9.Result);
         }
 
         public static async Task<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10)>
@@ -64,9 +72,10 @@
             Task<T1> task1, Task<T2> task2, Task<T3> task3, Task<T4> task4, Task<T5> task5,
             Task<T6> task6, Task<T7> task7, Task<T8> task8, Task<T9> task9, Task<T10> task10)
         {
-            await AggregateExceptions(task1, task2, task3, task4, task5, task6, task7, task8, task9, task10);
-            return (await task1, await task2, await task3, await task4, await task5,
-                    await task6, await task7, await task8, await task9, await task10);
+            await AggregateExceptions(task1, task2, task3, task4, task5, task6, task7, task8, task9, task10)
+                .ConfigureAwait(false);
+            return (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result,
+                    task6.Result, task7.Result, task8.Result, task9.Result, task10.Result);
         }
 
         public static async Task<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11)>
@@ -75,10 +84,11 @@
             Task<T6> task6, Task<T7> task7, Task<T8> task8, Task<T9> task9, Task<T10> task10,
             Task<T11> task11)
         {
-            await AggregateExceptions(task1, task2, task3, task4, task5, task6, task7, task8, task9, task10, task11);
-            return (await task1, await task2, await task3, await task4, await task5,
-                    await task6, await task7, await task8, await task9, await task10,
-                    await task11);
+            await AggregateExceptions(task1, task2, task3, task4, task5, task6, task7, task8, task9, task10, task11)
+                .ConfigureAwait(false);
+            return (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result,
+                    task6.Result, task7.Result, task8.Result, task9.Result, task10.Result,
+                    task11.Result);
         }
 
         public static async Task<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12)>
@@ -88,10 +98,11 @@
             Task<T11> task11, Task<T12> task12)
         {
             await AggregateExceptions(task1, task2, task3, task4, task5, task6, task7, task8, task9, task10,
-                                task11, task12);
-            return (await task1, await task2, await task3, await task4, await task5,
-                    await task6, await task7, await task8, await task9, await task10,
-                    await task11, await task12);
+                                    task11, task12)
+                .ConfigureAwait(false);
+            return (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result,
+                    task6.Result, task7.Result, task8.Result, task9.Result, task10.Result,
+                    task11.Result, task12.Result);
         }
 
         public static async Task<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13)>
@@ -101,10 +112,11 @@
             Task<T11> task11, Task<T12> task12, Task<T13> task13)
         {
             await AggregateExceptions(task1, task2, task3, task4, task5, task6, task7, task8, task9, task10,
-                                task11, task12, task13);
-            return (await task1, await task2, await task3, await task4, await task5,
-                    await task6, await task7, await task8, await task9, await task10,
-                    await task11, await task12, await task13);
+                                    task11, task12, task13)
+                .ConfigureAwait(false);
+            return (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result,
+                    task6.Result, task7.Result, task8.Result, task9.Result, task10.Result,
+                    task11.Result, task12.Result, task13.Result);
         }
 
         public static async Task<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14)>
@@ -114,10 +126,11 @@
             Task<T11> task11, Task<T12> task12, Task<T13> task13, Task<T14> task14)
         {
             await AggregateExceptions(task1, task2, task3, task4, task5, task6, task7, task8, task9, task10,
-                                task11, task12, task13, task14);
-            return (await task1, await task2, await task3, await task4, await task5,
-                    await task6, await task7, await task8, await task9, await task10,
-                    await task11, await task12, await task13, await task14);
+                                    task11, task12, task13, task14)
+                .ConfigureAwait(false);
+            return (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result,
+                    task6.Result, task7.Result, task8.Result, task9.Result, task10.Result,
+                    task11.Result, task12.Result, task13.Result, task14.Result);
         }
 
         public static async Task<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15)>
@@ -127,10 +140,11 @@
             Task<T11> task11, Task<T12> task12, Task<T13> task13, Task<T14> task14, Task<T15> task15)
         {
             await AggregateExceptions(task1, task2, task3, task4, task5, task6, task7, task8, task9, task10,
-                                task11, task12, task13, task14, task15);
-            return (await task1, await task2, await task3, await task4, await task5,
-                    await task6, await task7, await task8, await task9, await task10,
-                    await task11, await task12, await task13, await task14, await task15);
+                                    task11, task12, task13, task14, task15)
+                .ConfigureAwait(false);
+            return (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result,
+                    task6.Result, task7.Result, task8.Result, task9.Result, task10.Result,
+                    task11.Result, task12.Result, task13.Result, task14.Result, task15.Result);
         }
 
         public static async Task<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16)>
@@ -141,11 +155,12 @@
             Task<T16> task16)
         {
             await AggregateExceptions(task1, task2, task3, task4, task5, task6, task7, task8, task9, task10,
-                                task11, task12, task13, task14, task15, task16);
-            return (await task1, await task2, await task3, await task4, await task5,
-                    await task6, await task7, await task8, await task9, await task10,
-                    await task11, await task12, await task13, await task14, await task15,
-                    await task16);
+                                    task11, task12, task13, task14, task15, task16)
+                .ConfigureAwait(false);
+            return (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result,
+                    task6.Result, task7.Result, task8.Result, task9.Result, task10.Result,
+                    task11.Result, task12.Result, task13.Result, task14.Result, task15.Result,
+                    task16.Result);
         }
 
         private static async Task AggregateExceptions(params Task[] tasks)
